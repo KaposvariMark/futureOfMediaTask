9584253d97ec66d204f25040ca721897ae9d5ca7
Márk Kaposvári <kaposvari.markz@gmail.com>
2021-09-10 16:37:05 +0200

    FOMT04 - added necessary annotations to contactperson class

--- src/main/java/hu/futureofmedia/task/contactsapi/entities/ContactPerson.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/entities/ContactPerson.java
@@ -1,6 +1,10 @@
 package hu.futureofmedia.task.contactsapi.entities;
 
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +12,10 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class ContactPerson {
 
     @Id
@@ -24,4 +32,6 @@ public class ContactPerson {
 
     private LocalDate created;
     private LocalDate modified;
+
+
 }
